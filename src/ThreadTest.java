e521004231b711c6a65a5d2c3a14f6e589dea8e7
xl <zjlsxulei@gmail.com>
2015-07-14 00:35:11 +0800

    添加ThreadFactory的测试实例

--- src/ThreadTest.java
+++ src/ThreadTest.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.ThreadFactory;
+
 /**
  * Created by xl on 15/7/13.
  */
@@ -24,3 +30,35 @@ public class ThreadTest {
         }
     }
 }
+class MyThreadFactory implements ThreadFactory{
+
+    private int count;
+    private String name;
+    private List<String> stas;
+
+    public MyThreadFactory( String name ){
+        this.name = name;
+        this.count = 0;
+        this.stas = new ArrayList<String>();
+    }
+
+    @Override
+    public Thread newThread(Runnable r) {
+        Thread thread = new Thread(r, "Thread name : " + name + "; count : " + count);
+        count++;
+        stas.add(String.format("Thread %d : %s at %s", thread.getId(), thread.getName(), new Date()));
+        return thread;
+    }
+
+    public String getStas(){
+        StringBuffer stringBuffer = new StringBuffer();
+        Iterator<String> stringIterator = this.stas.iterator();
+        while (stringIterator.hasNext()){
+            stringBuffer.append(stringIterator.next());
+            stringBuffer.append("\n");
+        }
+        return stringBuffer.toString();
+    }
+}
+
+
